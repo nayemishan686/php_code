ff1fbf64cd28df38d47b4002f2ecfaf785b18c5b
Nayem <nayemishan377@gmail.com>
2022-05-07 00:00:28 +0600

    mail send byp authenticate

--- mailing/phpmailer.php
+++ mailing/phpmailer.php
@@ -1,12 +1,22 @@
 <?php
 require_once('phpmailer/PHPMailer.php');
 require_once('phpmailer/Exception.php');
+require_once('phpmailer/SMTP.php');
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 $obj = new PHPMailer(true);
 try{
+    $obj->SMTPDebug = 2;
+    $obj->isSMTP(true);
+    $obj->Host = "mail.server.com";
+    $obj->SMTPAuth = true;
+    $obj->Username = "youremail@domain.com";
+    $obj->Password = "*********";
+    $obj->SMTPSecure = "tls";
+    $obj->Port = 587;
+
     $obj->setFrom("nayem@gmail.com");
     $obj->addAddress("nayemishan377@gmail.com");
     $obj->isHTML(true);
